adc3c86b65684a6e32e0b06cf8b2e96d4a26e75a
DeusaThemis <deusathemis.tuj@gmail.com>
2020-02-10 12:45:07 -0600

    Fixed compiler error
    gave velocity a definition

--- Assets/Scripts/CookieScript.cs
+++ Assets/Scripts/CookieScript.cs
@@ -10,7 +10,7 @@ public class CookieScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.velocity
+        //this.velocity;
     }
 
     // Update is called once per frame
