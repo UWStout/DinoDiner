dfab596eb7f4f9d9047f0de86fec00fb05a3f919
DeusaThemis <deusathemis.tuj@gmail.com>
2020-02-10 14:17:38 -0600

    Cookie, Customer and GameManager
    UI, cookie-customer interaction, and established GM

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //static allows this to be accessed by anything else in the scene;
+    public static GameManager instance = null;
+
+    public int score = 0;
+    public Text scorecountText;
+
+    //awake is called before start
+    private void Awake()
+    {
+        //check if instance already exists
+        if (instance == null)
+        {
+            //if not, set it to this one we've created
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            //if a gamemanager already exists, destroy this one we'vev created
+            //this enforces the singleton pattern, meaning there can only be one instnace of the GameManager
+            Destroy(this.gameObject);
+        }
+
+        //set this to not be destroyed when going between scenes
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scorecountText = GameObject.Find("ScoreCountText").GetComponent<Text>();
+        setScoreCountText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void setScoreCountText()
+    {
+        scorecountText.text = "Score: " + score.ToString();
+    }
+
+}
