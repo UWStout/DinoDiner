8f07060b6918af9f7787d9b8c904afff5e2e6ab2
DeusaThemis <deusathemis.tuj@gmail.com>
2020-02-10 12:24:22 -0600

    Colliders
    Cookie and customer colliders, and some organization

--- Assets/Scripts/CookieScript.cs
+++ Assets/Scripts/CookieScript.cs
@@ -2,32 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CustomerMovementScript: MonoBehaviour
+public class CookieScript : MonoBehaviour
 {
-    private Vector3 origin;
-    private Vector3 destination;
-    public int timetoendsec = 5;
+    public int speed = 5;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //origin = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-        InvokeRepeating("moveForward", 1f, 1f);
+        this.velocity
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-
-    private void moveForward()
-    {
         this.transform.position = new Vector3(this.transform.position.x + 2, this.transform.position.y, this.transform.position.z);
+
     }
-
-
-
-
-
 }
