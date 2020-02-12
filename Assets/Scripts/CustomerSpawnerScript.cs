729ed6efd170bcafd79b2655b47aa55172d4f2b3
DeusaThemis <deusathemis.tuj@gmail.com>
2020-02-12 13:18:36 -0600

    Random Customer Spawning

--- Assets/Scripts/CustomerSpawnerScript.cs
+++ Assets/Scripts/CustomerSpawnerScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CustomerSpawnerScript : MonoBehaviour
 {
+    private int spawnrate;
+    public GameObject[] pool;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(spawnCustomer());
     }
+       
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator spawnCustomer()
+    {
+        int wait_time;
+        while (true)
+        {
+            wait_time = Random.Range(0, 10);
+            yield return new WaitForSeconds(wait_time);
+            Instantiate(pool[Random.Range(0, 3)], this.transform.position, this.transform.rotation);
+        }
     }
 }
