e23309e9225df1155f44cd2bd71d477b257153e0
Nathan Muck <nathanm250@gmail.com>
2020-02-12 13:05:50 -0600

    player movement changed and gather cookies from the ovens

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -11,7 +11,10 @@ public class PlayerControl : MonoBehaviour
     private float vely;
     private float friction;
     private float changespeed;
-   // public GameManager gameManager;
+    // public GameManager gameManager;
+
+    private float BigCook;
+    private float SmallCook;
 
 
 
@@ -21,8 +24,9 @@ public class PlayerControl : MonoBehaviour
         rb2D = GetComponent<Rigidbody2D>();
         speed = 3.0f;
         friction = .8f;
-        changespeed = 1f;
         //anim = GetComponent<Animator>();
+        BigCook = 0;
+        SmallCook = 0;
     }
 
     // Update is called once per frame
@@ -40,57 +44,33 @@ public class PlayerControl : MonoBehaviour
         {
            
 
-            vely += changespeed;
-            if (vely > speed)
-            {
-                if (vely < 0)
-                {
-                    vely = 0;
-                }
-                vely = speed;
-                
-            }
+            vely += speed;
+
         }
         else if (Input.GetAxisRaw("Vertical") < 0)
             {
-            vely -= changespeed;
-            if (vely < -speed)
-            {
-                if (vely > 0)
-                {
-                    vely = 0;
-                }
-                vely = -speed;
-            }
+            vely -= speed;
+
         }
         else
         {
             vely = 0;
         }
 
+
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
-            velx += changespeed;
-            if (velx > speed)
-            {
-                if (velx < 0)
-                {
-                    velx = 0;
-                }
-                velx = speed;
-            }
+            velx += speed;
+
         }
         else if (Input.GetAxisRaw("Horizontal") < 0)
         {
-            velx -= changespeed;
-            if (velx < -speed)
-            {
-                if (velx > 0)
-                {
-                  velx = 0;
-                }
-                velx = -speed;
-            }
+            velx -= speed;
+
+        }
+        else
+        {
+            velx = 0;
         }
 
         //do the angle between velx and vely
@@ -98,33 +78,23 @@ public class PlayerControl : MonoBehaviour
        //Vector2.Angle((velx,0.0f), vely);
 
 
-        rb2D.AddForce(new Vector2(velx, vely));
-        print("velx" + velx);
-        print("vely" + vely);
+        rb2D.velocity = new Vector2(velx, vely);
+
 
     }
 
-
-    private void OnTriggerEnter2D(Collider2D collision)
+   
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "ExitLeft")
+        if (collision.gameObject.name == "Smalltrigger" && Input.GetKeyDown(KeyCode.E))
         {
-            transform.position = new Vector2(13.5f, -0.5f);
+            BigCook += 1;
         }
-        else if (collision.gameObject.name == "ExitRight")
+        else if (collision.gameObject.name == "BigTrigger" && Input.GetKeyDown(KeyCode.E))
         {
-            transform.position = new Vector2(-13.5f, -0.5f);
-        }
-        else if (collision.gameObject.tag == "SmallPellet")
-        {
-
-        else if (collision.gameObject.tag == "BigPellet")
-        {
-            collision.gameObject.SetActive(false);
+            SmallCook += 1;
         }
 
     }
-
-
 
 }
