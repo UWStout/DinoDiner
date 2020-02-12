514033c23471f5639509a2efb764c17e5f1b2db7
Nathan Muck <nathanm250@gmail.com>
2020-02-12 12:38:55 -0600

    commit
    d

---
+++ Assets/Scripts/CookieScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookieScript : MonoBehaviour
+{
+    public int speed = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.velocity
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x + 2, this.transform.position.y, this.transform.position.z);
+
+    }
+}
---
+++ Assets/Scripts/CustomerMovementScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerMovementScript: MonoBehaviour
+{
+    private Vector3 origin;
+    private Vector3 destination;
+    public int timetoendsec = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //origin = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        InvokeRepeating("moveForward", 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void moveForward()
+    {
+        this.transform.position = new Vector3(this.transform.position.x + 2, this.transform.position.y, this.transform.position.z);
+    }
+
+
+
+
+
+}
