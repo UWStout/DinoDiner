e50bc6969d4df4f169d9c873c1b00cad53546649
Tommy Xiong <Xiongt1113@my.uwstout.edu>
2020-02-21 02:07:35 -0600

    txpull

--- Assets/Scripts/FillBarScript.cs
+++ Assets/Scripts/FillBarScript.cs
@@ -10,7 +10,9 @@ public class FillBarScript : MonoBehaviour
     //public Text displayText;
     public OvenScript linkedOven;
     public Image fillBar;
-
+    private Color tempBarcolor = new Color32(0,1,0,1);
+    public Image handleImage;
+    private float opNum = 0;
     private float currentValue = 1f;
     public float CurrentValue
     {
@@ -37,12 +39,38 @@ public class FillBarScript : MonoBehaviour
     {
         if (linkedOven.baking)
         {
-            CurrentValue = (linkedOven.timeLeft / linkedOven.bakeSpeed);
-            if (CurrentValue < 0)
-            {
-                //slowly turn the bar red
-                //temp comment
+            if (!linkedOven.burning) {
+                CurrentValue = (linkedOven.timeLeft / linkedOven.bakeSpeed);
             }
+            else if(linkedOven.burning)
+            {
+                CurrentValue = -1 * (linkedOven.timeLeft / linkedOven.bakeSpeed);
+                opNum = CurrentValue / 50;
+                print(opNum);
+                tempBarcolor.r += opNum;
+                tempBarcolor.g -= opNum; //color is from 0 to 1, so we need to divide Currentvalue by a larger number to get it in that range for gradual coloration
+                tempBarcolor.b = 0;
+                tempBarcolor.a = 1;
+                fillBar.color = tempBarcolor;
+
+            }
+            else if (linkedOven.burned)
+            {
+                tempBarcolor.r = 1;
+                tempBarcolor.g = 0;
+                tempBarcolor.b = 0;
+                tempBarcolor.a = 1;
+                fillBar.color = tempBarcolor;
+            }
+        }
+        else
+        {
+            CurrentValue = 1f;
+            tempBarcolor.r = 0;
+            tempBarcolor.g = 1;
+            tempBarcolor.b = 0;
+            tempBarcolor.a = 1;
+            fillBar.color = tempBarcolor;
         }
     }
 }
